33c16b9b9faf29682cca127ca17f73805dd14bb8
Syed Yusuf Ahmad <syeddyeah@gmail.com>
2023-05-28 18:36:23 +0530

    y-hot-100-Webapp: YouTube and Spotify combined with song analytics.

---
+++ youtube_links.py
@@ -0,0 +1,4 @@
+youtube_embed_src1="https://www.youtube.com/embed/videoseries?list=TLGGZDzoAW2I3jYyMDExMjAyMg"
+youtube_embed_src2="https://www.youtube.com/embed/videoseries?list=TLGGDVSI6Denuo8yMDExMjAyMg"
+playlist_link_1="https://www.youtube.com/watch?v=OjecIllOEio&list=TLGGZDzoAW2I3jYyMDExMjAyMg"
+playlist_link_2="https://www.youtube.com/watch?v=HXN7IUYf0vk&list=TLGGDVSI6Denuo8yMDExMjAyMg"
